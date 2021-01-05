a4025232c2ae49334924c9c1a7c01607a5f1e55a
R6LHL <69074926+R6LHL@users.noreply.github.com>
2021-01-05 21:27:37 +0300

    Add files via upload

--- Potentiometer.h
+++ Potentiometer.h
@@ -2,34 +2,43 @@
 #define _POTENTIOMETER_H
 
 #include <Arduino.h>
-#include <math.h>
 
-typedef unsigned const char pin_number;
+typedef unsigned char pin_num;
 
 template <unsigned const int T_ADC> // bit depth of ADC
 
 class Potentiometer
 {
 	public:
-			Potentiometer(pin_number p){
-				pin = p;
-				PinMode(pin, INPUT_PULLUP);
+			Potentiometer(pin_num p){
+				analog_pin = p;
+				pinMode(analog_pin, INPUT);
 				}
 				
-			Potentiometer (pin_number p, float voltage){
-				pin = p;
-				PinMode(pin, INPUT_PULLUP);
+			Potentiometer (pin_num p, float voltage){
+				analog_pin = p;
+				pinMode(analog_pin, INPUT);
 				AREF_VOLTAGE = voltage;
 				}
 			
-			//double getValueAsCoeff(void) {return ceil((analogRead(pin)/(T_ADC - 1));}
-			float getValueAsCoeff(void) const {return (float)(ceil((analogRead(pin)/(T_ADC - 1)));}
-			unsigned char getValueAsPercent(void) const {return ((analogRead(pin)/(T_ADC - 1))*100);}
-			unsigned int getValue(void) const {return analogRead(pin);}
-			float getVoltage(void) const {return ((analogRead(pin) / (T_ADC - 1)) * AREF_VOLTAGE);}
+			//double getValueAsCoeff(void) {return ceil((analogRead(analog_pin)/(T_ADC - 1));}
+			float getValueAsCoeff(void){
+			
+				int adc = analogRead(analog_pin);
+				int adc_bit_depth = (T_ADC - 1);
+				return (float)adc / (float)adc_bit_depth;
+			}
+			
+			unsigned char getValueAsPercent(void){return ((analogRead(analog_pin)/(T_ADC - 1))*100);}
+			unsigned int getValue(void) {return analogRead(analog_pin);}
+			float getVoltage(void) {
+				int adc = analogRead(analog_pin);
+				int adc_bit_depth = (T_ADC - 1);
+				return ((float)(analogRead(analog_pin) / (float)(T_ADC - 1)) * AREF_VOLTAGE);
+				}
 			
 	private: 
-			pin_number analog_pin;
+			pin_num analog_pin;
 			float AREF_VOLTAGE;
 			
 			
